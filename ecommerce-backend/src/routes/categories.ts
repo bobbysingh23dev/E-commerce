adaf33022770f89c1e94dbdfbe3f0d89704d50bf
Bobby Singh <bobbysingh23dev@gmail.com>
2026-09-16 14:49:50 +0530

    feat: add authentication middleware to category routes

--- ecommerce-backend/src/routes/categories.ts
+++ ecommerce-backend/src/routes/categories.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import * as categoryController from "../controllers/categories";
 import { validateCategoryBody } from "../middlewares/validateCategoryBody";
 import { validateId } from "../middlewares/validateId";
-
+import { authenticate } from "../middlewares/authenticate";
 const categoriesRouter = Router();
 
 categoriesRouter.post(
   "/",
+  authenticate,
   validateCategoryBody,
   categoryController.createCategory,
 );
@@ -17,8 +18,14 @@ categoriesRouter.put(
   "/:id",
   validateCategoryBody,
   validateId,
+  authenticate,
   categoryController.updateCategoryById,
 );
-categoriesRouter.delete("/:id", validateId, categoryController.deleteCategory);
+categoriesRouter.delete(
+  "/:id",
+  validateId,
+  authenticate,
+  categoryController.deleteCategory,
+);
 
 export default categoriesRouter;

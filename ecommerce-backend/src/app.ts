900f46fef363e956187f33958b6d33d2e97f95bb
Bobby Singh <bobbysingh23dev@gmail.com>
2026-09-23 16:15:38 +0530

    feat: add pino logging with pino-http middleware for improved logging

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -5,7 +5,8 @@ import swaggerUi from "swagger-ui-express";
 import routes from "./routes";
 import { errorHandler } from "./middlewares/errorHandler";
 import { openapiDocument } from "./openapi";
-
+import { pinoHttp } from "pino-http";
+import { logger } from "./config/logger";
 dotenv.config();
 
 // The Express app, WITHOUT app.listen(). Exported so tests (supertest) can use
@@ -15,6 +16,7 @@ export const app = express();
 // ---- Global middleware ----
 app.use(cors());
 app.use(express.json());
+app.use(pinoHttp({ logger }));
 
 // ---- API docs (Swagger UI) ----
 // openapiDocument = the hand-written openapi.yaml with the cart input schemas
